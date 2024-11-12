6d2f9c71bd5a0382b89fa27521827192ce8b0d8d
nitesh <nitesh.khatri88@gmail.com>
2024-11-12 18:28:18 +0530

    updated

---
+++ daily-update/12112024.js
@@ -0,0 +1,135 @@
+// let score = 50.12
+// console.log(score);
+
+// let name ='test'
+// console.log(name)
+// let example
+// example = 'gappy'
+// console.log(example)
+
+// let totalscore = (5+3)*10
+// console.log(totalscore)
+
+// let x,y 
+// x = 5 
+// y = 10
+// console.log(x,y)
+
+// document.write('hello world javascript')
+
+// object
+// const person =  {
+//     firstName:'test1',
+//     lastName:'test2'
+// }
+// console.log(person.firstName)
+// console.log(person.lastName)
+
+// array objects 
+// const cars = ['maruti','innova','sumo','safari','esteem','sumo']
+// console.log([cars[0]].concat(' ',[...cars]))
+
+// const date = new Date('2024-11-12')
+// console.log(date)
+
+// let scores = [
+//     {name:'test1',score:55},
+//     {name:'test2',score:75},
+// ]
+// console.log(JSON.stringify(scores[1]))
+
+// const test1 = 'happy'
+// const test2 = [1,2,2,34]
+// const test3 = {name:'tst'}
+// const test4 = 1111
+// const test5 = true 
+// console.log(typeof test1)
+// console.log(typeof test2)
+// console.log(typeof test3)
+// console.log(typeof test4)
+// console.log(typeof test5)
+
+// function myFunction(p1,p2){
+//     return p1 * p2 
+// }
+// console.log(myFunction(10,20))
+
+// const example = (a,b) => {
+//     return a * b 
+// }
+// console.log(example(200,300))
+
+// function greet(fname,lname,city,state){
+//     return `${fname} ${lname} ${city} ${state}`
+// }
+// console.log(greet('test1','test2','maharashtra','mumbai'))
+
+// function summer(n1,n2,n3){
+//     return n1 + n2 * n3 
+// }
+// console.log(summer(10,20,30))
+
+// const person1 = {
+//     fname:'test1',
+//     lname:'test2',
+//     age:35,
+//     eyeColor:'black',
+//     getFullName:function(){
+//         return this.fname + " " + this.lname
+//     }
+// }
+// console.log(person1.fname)
+// console.log(person1.lname)
+// console.log(person1.age)
+// console.log(person1.eyeColor)
+// console.log(person1.getFullName());
+
+// let fruits = ['apple','pineapple','banana','grape','blueberry','mosambi']
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+// for(let i=0;i<fruits.length;i++){
+//     console.log(i + " " + fruits[i])
+// }
+
+// for(const fruit of fruits){
+//     console.log(fruit)
+// }
+
+// const numbers = [1,2,3,4,5,6]
+// console.log(numbers);
+// const double = (numbers) => {
+//     let result = []
+//     for(const num of numbers){
+//         result.push(num * 2)
+//         return result 
+//     }
+// }
+// console.log(double([11,222,333,4,5,6]))
+
+// const howManyLetters = () => {
+//     const phrase = 'hey, can you go to grocery store with me?'
+//     for(letter of phrase){
+//         console.log(Number(letter) + 1)
+//     }
+// }
+// howManyLetters();
+
+// const letterFrequencey = (phrase) => {
+//     for(let example of phrase){
+//         console.log(example)
+//     }
+// }
+// letterFrequencey('nitesh khatri')
+
+// [1,2,3,4,5].map((num) => console.log(num))
+// [1,2,3,4,5].filter((num) => console.log('Even', num % 2 !== 0));
+// const doubleMap = (numbers) => {
+//     return numbers.map(number => number * 2)
+// }
+// console.log(doubleMap([1,2,3,4]))
+
+
+
+

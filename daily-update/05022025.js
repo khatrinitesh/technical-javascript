e2c288d47282496814763fd19198038926314e20
nitesh <nitesh.khatri88@gmail.com>
2025-02-05 18:23:35 +0530

    updated

---
+++ daily-update/05022025.js
@@ -0,0 +1,13 @@
+// console.log('hi nitesh')
+
+// const users = [
+//     {id:1,name:'test1',amount:3400},
+//     {id:2,name:'test2',amount:4400},
+//     {id:3,name:'test3',amount:5400},
+//     {id:4,name:'test4',amount:7400},
+//     {id:5,name:'test5',amount:8400},
+// ]
+// console.log(users);
+// const usersResult = users.filter((val) => val.amount > 4500 || val.name === 'test1')
+// console.log(usersResult)
+

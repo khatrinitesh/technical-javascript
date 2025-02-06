2fd80cdfb0ac5bb6fa8f92c1d89db5533eb8b3d6
nitesh <nitesh.khatri88@gmail.com>
2025-02-06 15:00:58 +0530

    updated

---
+++ daily-update/06022025.js
@@ -0,0 +1,163 @@
+// var x = 1;
+// if(x === 1){
+//     var x= 2
+//     console.log(x)
+// }
+// console.log(x)
+
+// let x1 = 1
+// if(x1 === 2){
+//     let x1 = 2;
+//     console.log(x1)
+// }
+// console.log(x1)
+
+// const number = 48;
+// try{
+//     const number = 42;
+// }
+// catch(err){
+//     console.log(err)
+// }
+// console.log(number)
+
+// const word = 'happy'
+// try{
+//     const word = 'smile'
+//     console.log(word)
+// }
+// catch(err){
+//     console.log(err)
+// }
+// console.log(word)
+
+// let person = "Nick";
+// person = "John";
+// console.log(person)
+
+// let humanbody = 'vishal'
+// humanbody = 'sameet'
+// console.log(humanbody)
+
+// const age = 45
+// switch(age){
+//     case 45: console.log('x is 45')
+//     break;
+//     case 30: console.log('x is 30')
+//     break;
+//     default:console.log(' it is not match')
+//     break;
+// }
+
+// const day = new Date().getDay();
+// const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+// const dayName = daysOfWeek[day];
+// switch(day){
+//     case 'sunday':console.log('sunday')
+//     break;
+//     case 'saturday':console.log('saturday')
+//     break;
+//     case 'monday':console.log('monday')
+//     break;
+//     case 'tuesday':console.log('tuesday')
+//     break;
+//     case 'wednesday':console.log('wednesday')
+//     break;
+//     case 'thursday':console.log('thursday')
+//     break;
+//     case 'friday':console.log('friday')
+//     break;
+//     default:console.log('There is no such day')
+//     break;
+// }
+
+// else if
+// const size = 120;
+// if(size > 100){
+//     console.log('Big');
+// } else if (size > 20) {
+//   console.log('Medium');
+// } else if (size > 4) {
+//   console.log('Small');
+// } else {
+//   console.log('Tiny');
+// }
+
+// switch statement
+
+// const food = "salad";
+// switch (food) {
+//   case "oyster":
+//     console.log("The taste of the sea");
+//     break;
+//   case "pizza":
+//     console.log("A delicious pie");
+//     break;
+//   default:
+//     console.log("Enjoy your meal");
+// }
+
+// **************** javascript conditionals
+// const isMailSent = true 
+// if(isMailSent){
+//     console.log('yes')
+// }
+// else{
+//     console.log('no')
+// }
+// operators 
+// console.log(true || false);
+// console.log(10 > 5 || 10 > 20);
+// console.log(false || false);
+// console.log(10 > 100 || 10 > 20)
+
+// var x = 1
+// const result = x === 1 ? 'y' : 'n'
+// console.log(result)
+
+
+// logical operator
+// console.log(true && true)
+// console.log(1 > 2 && 2 > 1)
+// console.log(true && false)
+// console.log(4 === 4 && 3 > 1)
+
+// comparison operators 
+// console.log(1 > 3)
+// console.log(1 > 2 && 2 > 1)
+// console.log(true && false)
+// console.log(4 === 4 && 3 > 1)
+
+// console.log(null ?? 'i win')
+// console.log(undefined ?? 'me too')
+// console.log(false ?? 'i lose')
+// console.log(0 ?? 'i lose again')
+// console.log('' ?? 'damn it')
+
+// console.log(0 == false)
+// console.log(0 === false)
+// console.log(1 == '1')
+// console.log(1 === '1')
+// console.log(null == undefined)
+// console.log(null === undefined)
+// console.log('0' == false)
+// console.log('0' === false)
+
+// javascript functions ***********************
+// functions
+// function sum(n1,n2){
+//     return n1 + n2 
+// }
+// console.log(sum(10,20))
+// anonymous functions 
+// function rocketToMars(){
+//     return 'BOOM!'
+// }
+// console.log(rocketToMars())
+// const example = function(){
+//     return 'Happy'
+// }
+// console.log(example())
+
+// arrow functions (ES6)
+

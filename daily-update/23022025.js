ce1aa993249452689f935ac1f65d10cea4d2eecc
nitesh.khatri88 <nitesh.khatri88@gmail.com>
2025-02-23 14:00:03 +0530

    updated

---
+++ daily-update/23022025.js
@@ -0,0 +1,41 @@
+// function abc(){
+//     console.log('hiii')
+// }
+// abc()
+
+// let arr = [1,2,3,4,5]
+// const result = arr.reduce((item,acc) => acc + item)
+// console.log(result)
+
+// if([]){
+//     console.log('i am present')
+// }
+// else{
+//     console.log('i am absent')
+// }
+
+
+// let str = 'hello happy'
+// // console.log(str)
+// let res = ''
+// for(let i=str.length-1;i>=0;i--){
+//     res = res + str.charAt(i)
+// }
+// console.log(res)
+
+// let arr = [2,4,8,10]
+// const [a,b] =arr 
+// console.log(a,b)
+
+// let age = 19
+// if(age > 20){
+//     console.log('above')
+// }
+// else{
+//     console.log('below')
+// }
+// console.log(age > 20 ? 'yes' : 'no')
+
+
+
+

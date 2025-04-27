00b552429396b489c7cfac86bc0d5867e98e48f2
nitesh.khatri88 <nitesh.khatri88@gmail.com>
2025-04-27 11:28:44 +0530

    updated

---
+++ daily-update/26042025.js
@@ -0,0 +1,31 @@
+// let arr = [1,2,3]
+// console.log(arr2.indexOf(3))
+// console.log(arr.push(4))
+// let arr2 = new Array(1,2,3);
+
+
+// let str = 'this is nitesh'
+// function demo(str){
+//     for(let i=0;i<str.lengthi;i++){
+//         if(str[i] === ' '){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// function demo(a,...b){
+//     return [a,b]
+// }
+// console.log(demo(1,222,333,45,58,66))
+
+// let [a,b] = [1,55,3,4]
+// console.log(a)
+// console.log(b)
+
+// let x;
+// x = 10
+// console.log(x)
+// let x = 20
+// const arr = [1,2,3,4,5]
+// const newArr = arr.filter(num => num>2)
+// console.log(newArr)
